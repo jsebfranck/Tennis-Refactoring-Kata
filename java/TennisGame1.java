30fbf447bf004fec0a74e6b3eca17e0332ab373d
jsebfranck <jsebfranck@gmail.com>
2013-11-17 14:25:25 +0100

    Renamed score attributes

--- java/TennisGame1.java
+++ java/TennisGame1.java
@@ -1,10 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,15 +13,15 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == player1Name)
-            m_score1 += 1;
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
-        if (m_score1 == m_score2) {
+        if (scorePlayer1 == scorePlayer2) {
             return getEqualityScore();
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
             return getAdvantageOrWinScore();
         } else {
             return getOtherScore();
@@ -32,10 +32,10 @@ public class TennisGame1 implements TennisGame {
         String score = "";
         int tempScore;
         for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = m_score1;
+            if (i == 1) tempScore = scorePlayer1;
             else {
                 score += "-";
-                tempScore = m_score2;
+                tempScore = scorePlayer2;
             }
             switch (tempScore) {
                 case 0:
@@ -57,7 +57,7 @@ public class TennisGame1 implements TennisGame {
 
     private String getAdvantageOrWinScore() {
         String score;
-        int minusResult = m_score1 - m_score2;
+        int minusResult = scorePlayer1 - scorePlayer2;
         if (minusResult == 1) score = "Advantage player1";
         else if (minusResult == -1) score = "Advantage player2";
         else if (minusResult >= 2) score = "Win for player1";
@@ -67,7 +67,7 @@ public class TennisGame1 implements TennisGame {
 
     private String getEqualityScore() {
         String score;
-        switch (m_score1) {
+        switch (scorePlayer1) {
             case 0:
                 score = "Love-All";
                 break;
